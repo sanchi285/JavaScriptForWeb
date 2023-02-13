7af3b503e8984b4ad54b2b3073ae5f04a9802780
sanchi285 <bismoy.sanchi@gmail.com>
2023-02-14 01:15:46 +0600

    Normalize urls and tests

--- crawl.test.js
+++ crawl.test.js
@@ -9,8 +9,23 @@ test('normalizeURL', ()=>{
 })
 
 
-test('normalize url strip protocol', ()=>{
+test('normalize url strip trailing slash', ()=>{
     const input = 'https://blogboot.dev/path/'
+    const actual = normalizeURL(input)
+    const expected = 'blogboot.dev/path' 
+    return expect(actual).toEqual(expected)
+})
+
+
+test('normalize Capital', ()=>{
+    const input = 'https://BLOGboot.dev/path/'
+    const actual = normalizeURL(input)
+    const expected = 'blogboot.dev/path' 
+    return expect(actual).toEqual(expected)
+})
+
+test('normalize strip http', ()=>{
+    const input = 'http://BLOGboot.dev/path/'
     const actual = normalizeURL(input)
     const expected = 'blogboot.dev/path' 
     return expect(actual).toEqual(expected)

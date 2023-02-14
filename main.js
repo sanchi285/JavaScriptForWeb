a0dd4f795459ba33e996bcc05db3f75fa150da62
sanchi285 <bismoy.sanchi@gmail.com>
2023-02-15 00:10:35 +0600

    main command line args

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+//console.log("Hello World");
+function main(){
+    if(process.argv.length<3){
+        console.log("no web site provided")
+        process.exit(1)
+    }
+  //  console.log("Starting crawl")
+    for(const arg of process.argv){
+        console.log(arg)
+    }
+}
+
+main()

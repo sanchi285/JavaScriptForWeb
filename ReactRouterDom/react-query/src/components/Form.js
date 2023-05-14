2318ae8bf475279da395e6c493e47f14e53f572c
sanchi285 <bismoy.sanchi@gmail.com>
2023-05-14 16:20:26 +0600

    Schema, yup,  resolver has been added

--- ReactRouterDom/react-query/src/components/Form.js
+++ ReactRouterDom/react-query/src/components/Form.js
@@ -1,7 +1,25 @@
 import { useForm } from "react-hook-form"
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup';
 export const Form = ()=>{
 
-    const {register, handleSubmit} = useForm();
+   
+    const schema  = yup.object().shape(
+      {
+        fullName: yup.string().required(),
+        email: yup.string().email().required(),
+        age: yup.number().positive().integer().min(18).required(),
+        password: yup.string().min(4).max(20).required(),
+        confPass: yup.string().oneOf([yup.ref("password"),null]).required(),
+        
+      }
+    );
+
+    const {register, handleSubmit} = useForm(
+      {
+        resolver: yupResolver(schema)
+      }
+    );
 
     const onSubmit = (data)=>{
         console.log(data);

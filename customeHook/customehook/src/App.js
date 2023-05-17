ad4d9c9e7222cd817a9fa0650fd982195b9069c1
sanchi285 <bismoy.sanchi@gmail.com>
2023-05-18 01:06:47 +0600

    Custome hook created

--- customeHook/customehook/src/App.js
+++ customeHook/customehook/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { useToggle } from './component/useToggle';
+
 
 function App() {
+
+const [state, setState] = useToggle();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <button onClick={()=>setState()}>
+      {state ? "Hide" : "Show"}
+     </button>
+     {state && <h1>Hide Text</h1>}
     </div>
   );
 }
